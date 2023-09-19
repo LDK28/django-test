2db130489eb076f17273a3e43c8c4e07c814a447
d.kostev <d.kostev@vk.team>
2023-09-19 14:18:06 +0300

    2 lesson

--- hello/urls.py
+++ hello/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from hello.views import index, about
+from hello.views import index, about, dude
 
 urlpatterns = [
     path('', index, name='home'),
-    path('about/', about, name='about')
+    path('about/', about, name='about'),
+    path('dude/<str:name>', dude, name='dude'),
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,7 +1,13 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('Hello World!')
+    return render(request, 'index.html')
 
 def about(request):
     return HttpResponse('<h2 style="color:blue;">About our app</h2>')
+
+def dude(request, name):
+    langs = ["Russian", "English", "French"]
+    data = {"name": name, "langs": langs, "showButton": False}
+    return render(request, 'dude.html', context=data)
